32e39599097b175ddf79d7b8fafa15322db5b636
Morten Larsen <morten.larsen@7sense.no>
2022-11-28 09:39:10 +0100

    Added:
    - 'fast_pony_crud' sub-project.

--- orm_ex1/pony1.py
+++ orm_ex1/pony1.py
@@ -1,7 +1,10 @@
-from pony.orm import Database, PrimaryKey, Required, Optional, Set, db_session
+from pony.orm import Database, PrimaryKey, Required, Optional, Set, db_session, set_sql_debug
 
 db = Database()
 db.bind('sqlite', ':memory:')   # In-memory SQLite DB only --> for testing ...
+
+# Enable SQL debug:
+#set_sql_debug()
 
 # Model:
 
@@ -14,7 +17,7 @@ class Sensor(db.Entity):
 class SensorHub(db.Entity):
     ser_no = PrimaryKey(int)
     name = Required(str)
-    bus_id = Optional(str)
+    bus_ids = Optional(str)
     sensors = Set(Sensor)
 
 # Create database tables:
@@ -28,9 +31,9 @@ def create_entities():
     adxl255 = Sensor(partno="ADXL255", bus_type="UART", unit_name="G")
     fxs3008 = Sensor(partno="FXS3008", bus_type="I2C", unit_name="Bar")
     # Then sensor-hub entities ...
-    SensorHub(ser_no=123, name="TestHub1", bus_id="SPI_0.3 UART0, I2C1.0x48", sensors=(bma380, adxl255, fxs3008))
-    SensorHub(ser_no=223, name="TestHub2", bus_id="SPI_0.1 UART3, I2C1.0x56", sensors=(bma380, adxl255, fxs3008))
-    SensorHub(ser_no=323, name="TestHub3", bus_id="SPI_3.0 UART1, I2C1.0x71", sensors=(bma380, adxl255, fxs3008))
+    SensorHub(ser_no=123, name="TestHub1", bus_ids="SPI_0.3, UART0, I2C1.0x48", sensors=(bma380, adxl255, fxs3008))
+    SensorHub(ser_no=223, name="TestHub2", bus_ids="SPI_0.1, UART3, I2C1.0x56", sensors=(adxl255, fxs3008))
+    SensorHub(ser_no=323, name="TestHub3", bus_ids="SPI_3.0, UART1, I2C1.0x71", sensors=(bma380, fxs3008))
 
 create_entities()
 db.commit()     # Redundant, done at exit from 'create_etities()' func)
@@ -39,12 +42,18 @@ db.commit()     # Redundant, done at exit from 'create_etities()' func)
 @db_session
 def retrieve_entities():
     peticular_hub = SensorHub[123]                              # Retrieve single entity
-    print(f"Name of the peticular hub: {peticular_hub.name}")
+    print(f"Name of hub w. serial no.123: {peticular_hub.name}\n")
+
 
     hubs = SensorHub.select(lambda hub: hub.ser_no > 200)       # Retrieve multiple entitities
     print("All hubs with serial no. over 200:")
     for hub in hubs:
         print(f"{hub.name}")
+        print(f"SerNo = {hub.ser_no}")
+        print(f"bus-ID(s) = {hub.bus_ids}")
+        print("Sensors connected:")
+        for sensor in hub.sensors:
+            print(f"\t{sensor.partno}")
 
 
 if __name__ == "__main__":
