52df13a213746adba884fd919b5caf8fb0124049
ml <morten.larsen@7sense.no>
2023-01-03 12:37:45 +0100

    Added:
    - module demonstrating use of Python 3.9's new std-library 'zoneinfo' for simpler TimeZone-handling w. DateTimes.

---
+++ orm_ex1/time_zone.py
@@ -0,0 +1,46 @@
+"""
+@brief Demo of 'timezone' std.library, used to generate time-zone info.
+
+@note Requires >= Python3.9 !
+"""
+
+from zoneinfo import ZoneInfo
+from datetime import datetime
+
+
+def show_datetime_info(dt_info: tuple, tz: str = None):
+    if tz is None:
+        print("No TimeZone info ...")
+        # Fallback 1:
+        dt = datetime(year, month, day, hour)
+    else:
+        try:
+            tzInfo = ZoneInfo(tz)
+            dt = datetime(year, month, day, hour, tzinfo=tzInfo)
+        except Exception as ex:
+            print(f"ERROR! Cause: {ex}")
+            # Fallback 2:
+            dt = datetime(year, month, day, hour)
+    #
+    print(f"DateTime: {dt}")
+    tzName = dt.tzname()
+    print(f"""TimeZone name: {"no TZ-name" if tzName is None else tzName}""")
+    #
+    print()
+
+
+if __name__ == "__main__":
+    year = 2022
+    month = 10
+    day = 31
+    hour = 12
+    dt_info = year, month, day, hour
+    #
+    show_datetime_info(dt_info=dt_info)
+    #
+    show_datetime_info(dt_info=dt_info, tz="America/Los_Angeles")
+    #
+    show_datetime_info(dt_info=dt_info, tz="Europe/Oslo")
+    #
+    show_datetime_info(dt_info=dt_info, tz="Luna/SeaOfTranquility")
+
